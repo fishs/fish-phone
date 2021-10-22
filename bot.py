51a9249a3bf4466c76ba750783a2acd0f859b36d
scott <sfish87@gmail.com>
2021-10-21 23:56:26 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'ex' in incoming_msg:
+        msg.body('no')
+        responded = True
+    if not responded:
+        msg.body('no')
+    return str(resp)
+
+if __name__ == '__main__':
+    app.run()
